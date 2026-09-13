f609c2995959698afd8add4aa0113adfc25d7d92
safedrivebrazil <safedrivebrazil@gmail.com>
2026-09-13 11:17:07 -0300

    login

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SiteController;
+use App\Http\Controllers\SiteController;    
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/welcome', function () {
     return view('welcome');
 });
 
+//SITE
 Route::get('/', [SiteController::class, 'index']);
 
+//LOGIN
+Route::get('/login', [LoginController::class, 'index']);
