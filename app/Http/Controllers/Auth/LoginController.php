67b5352dc2ce2e11bbc03df991a33ecf3ed44ae4
safedrivebrazil <safedrivebrazil@gmail.com>
2026-09-13 21:03:46 -0300

    organizacao, create logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
@@ -21,11 +22,22 @@ class LoginController extends Controller
 
         if(Auth::attempt($credentials)){
             $request->session()->regenerate();
-            return redirect()->intended('/');
-        }
+            return redirect()->intended(route('site.dashboard'));
+        } 
 
         return back()->withErrors([
             'email' => 'Credenciais inválidas.',
         ]);  
+    
     }
+
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('site.index');
+    }
+
 }
